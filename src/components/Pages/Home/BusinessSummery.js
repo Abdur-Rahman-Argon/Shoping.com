8d699dca8422a5713a7aff13792fafdd0d4f0924
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-07-01 21:16:26 +0600

    home point file

---
+++ src/components/Pages/Home/BusinessSummery.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BusinessSummery = () => {
+  return <div></div>;
+};
+
+export default BusinessSummery;
---
+++ src/components/Pages/Home/DeliveryProsess.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeliveryProsess = () => {
+  return <div></div>;
+};
+
+export default DeliveryProsess;
