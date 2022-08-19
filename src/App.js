95c47f40599cb560ee1729ca4fbec7fa074e88ed
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-19 20:30:47 +0600

    navber added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navber from "./components/Shared/Navber";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navber></Navber>
+    </div>
+  );
 }
 
 export default App;
