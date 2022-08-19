0e2ceec9e651429250f1d155d460b4310a1f8104
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-19 22:19:24 +0600

    all product load and show

---
+++ src/components/Pages/AllProducts/AllProducts.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useQuery } from "react-query";
+import useProduct from "../../utilites/useProduct";
+import Product from "./Product";
+
+const AllProducts = () => {
+  // all product  load
+  const {
+    data: products,
+    isLoading,
+    refetch,
+  } = useQuery("product", () =>
+    fetch("http://localhost:5000/allProducts").then((res) => res.json())
+  );
+
+  // const [products, isLoading] = useProduct();
+
+  if (isLoading) {
+    return;
+  }
+
+  // if (products) {
+  //   console.log(products);
+  // }
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {products.map((product) => (
+          <Product key={product._id} product={product}></Product>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
