67ecdc718404f0cd538ca989041a5318d8ed15d1
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-19 22:28:34 +0600

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Navber from "./components/Shared/Navber";
+import Home from "./components/Pages/Home/Home";
+import Login from "./components/Pages/Login/Login";
+import SignUp from "./components/Pages/Login/SignUp";
+import AllProducts from "./components/Pages/AllProducts/AllProducts";
 
 function App() {
   return (
     <div>
       <Navber></Navber>
+
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+
+        <Route path="/allProduct" element={<AllProducts></AllProducts>}></Route>
+      </Routes>
+
+      {/* Toast add */}
+      <ToastContainer />
     </div>
   );
 }
